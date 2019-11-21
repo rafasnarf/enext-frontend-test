3813caa7b19c8ea151cf562892e6ce476cd9f174
rafasnarf <snarfelcv@gmail.com>
2019-11-21 19:26:16 -0300

    Enext-Frontend

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,34 +1,44 @@
 import React from 'react';
 
 import './index.css';
-// eslint-disable-next-line import/no-webpack-loader-syntax
-import * as data from 'json-loader!../../assets/fakeApi.json';
+import * as fakeApi from '../../assets/fakeApi.json'
 
 class Section extends React.Component {
-  /*constructor(props) { 
-    super(props);
-    this.state = {
-      data: []
-    }
-  }*/
-  /*dados() {
-    let URL = fakeApi;
-    fetch(URL)
-      .then(function (response) {
-        let data = response.json()
-        return data;
-      })
-      .then((json) => {
-        console.log('message :', json)
-        this.setState({ data: json });
-      })
-      .catch(function (ex) {
-        console.log('parsing failed', ex)
-      })
-  }*/
-  render() {
+
+  renderPotion(row) {
     return (
-      console.log(data)
+      <ul>
+        <li id='image'>{row.image}</li>
+        <li id='name'>{row.name}</li>
+        <li id='price'>{row.price}</li>
+      </ul>)
+  }
+  render() {
+    let rows = [];
+    for (let i = 1; i < 7; i++) {
+      rows.push(fakeApi.potions[i])
+    }
+    return (
+      <div>
+        <ul>
+          {rows.map(this.renderPotion)}
+        </ul>
+      </div>
+      //<h1>{fakeApi.potions[1].name}</h1>
+      /*<div className='main-container'>
+        <h1>Potions</h1>
+        <ul>
+          {fakeApi.potions.map(id => (
+            <li key={fakeApi.potions.id}>
+              <img src={fakeApi.potions.image} alt={fakeApi.potions.name} />
+              <footer>
+                <strong>{fakeApi.potions.name}</strong>
+                <p>{fakeApi.potions.price}</p>
+              </footer>
+            </li>
+          ))}
+        </ul>
+      </div>*/
     )
   }
 
